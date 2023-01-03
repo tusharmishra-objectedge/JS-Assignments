67ba3b8f819c63a44cadff572ff51e8a7a110251
Tushar Mishra <114741541+tusharmishra-objectedge@users.noreply.github.com>
2023-01-03 21:44:01 +0530

    Update JS_Assignment_1.js

--- JS_Assignment_1.js
+++ JS_Assignment_1.js
@@ -1,12 +1,12 @@
 // 1
 function Employee(firstName, lastName) {
- 	return `${firstName} ${lastName}`
+  return `${firstName} ${lastName}`
 }
 console.log(Employee('object','edge'))
 
 // 2
 for(let i=1; i<=10; i++){
-	console.log(i)
+  console.log(i)
 }
 
 // 3
@@ -14,9 +14,12 @@ let a = [ 2 , 4, 8, 9, 10, 2, 5, 10, 12, 89]
 console.log(...new Set(a))
 
 // 4
-let a = [ 2 , 4, 8, 9, 10, 2, 5, 10, 12, 89]
-let date = new Date()
-while(true){
-	console.log(date.toLocaleTimeString())
-	date = new Date()
+function updateTime() {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let timeString = `${hours}:${minutes}:${seconds}`;
+  console.log(timeString);
 }
+setInterval(updateTime, 1000);
