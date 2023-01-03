957b93be13dcd742d5d8497d294ceaf550d1a534
Tushar Mishra <114741541+tusharmishra-objectedge@users.noreply.github.com>
2023-01-03 20:48:32 +0530

    Create JS_Assignment_2.js

--- JS_Assignment_2.js
+++ JS_Assignment_2.js
@@ -5,14 +5,17 @@ for (var i = 1; i <= arr.length; i++) {
 }
 
 // 2
-<button id="btn-0" onClick="alertMe(0)">Button 1!</button>
-<button id="btn-1" onClick="alertMe(1)">Button 2!</button>
-<button id="btn-2" onClick="alertMe(2)">Button 3!</button>
+<button id="btn-0">Button 1!</button>
+<button id="btn-1">Button 2!</button>
+<button id="btn-2">Button 3!</button>
 
 <script type="text/javascript">
     var prizes = ['mango', 'Apple', 'Strawberry'];
-
-    function alertMe(id) {
-        alert(prizes[id])
+    for (var btnNum = 0; btnNum < prizes.length; btnNum++) {
+        (function(index) {
+            document.getElementById('btn-' + index).onclick = function() {
+                alert(prizes[index]);
+            };
+        })(btnNum);
     }
 </script>
